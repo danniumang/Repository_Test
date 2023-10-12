211b19e1d8621657ddee7905a859b6d664a775c7
danniumang <109894216+danniumang@users.noreply.github.com>
2023-10-12 10:45:49 +0800

    boot  service aspect resource config sysrunn test

--- demo/src/main/java/com/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/DemoApplication.java
@@ -2,12 +2,17 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		//SpringApplicationBuilder builder=new SpringApplicationBuilder(DemoApplication.class);
+		//builder.run(args);
+		
 	}
 
 }
